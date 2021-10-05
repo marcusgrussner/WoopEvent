51417a495e74bfb8234932390ab1271b8ab2dcc4
Marcus Vinicius Grussner <marcusgrussner@Marcuss-MacBook-Pro-2.local>
2021-10-05 00:15:57 -0300

    add swift lint config

--- WoopEvent/SupportingFiles/SwiftGen/WPStrings.swift
+++ WoopEvent/SupportingFiles/SwiftGen/WPStrings.swift
@@ -1,8 +1,34 @@
-//
-//  WPStrings.swift
-//  WoopEvent
-//
-//  Created by Marcus Vinicius Grussner on 05/10/21.
-//
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
 
 import Foundation
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return
+
+// MARK: - Strings
+
+// swiftlint:disable function_parameter_count identifier_name line_length type_body_length
+internal enum WPStrings {
+}
+// swiftlint:enable function_parameter_count identifier_name line_length type_body_length
+
+// MARK: - Implementation Details
+
+extension WPStrings {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: nil, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type

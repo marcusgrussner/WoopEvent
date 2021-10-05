51b1ba17a020ffaff1218adba4aed8c95ba2408e
Marcus Vinicius Grussner <marcusgrussner@Marcuss-MacBook-Pro-2.local>
2021-10-04 23:47:31 -0300

    init new project

---
+++ WoopEventTests/WoopEventTests.swift
@@ -0,0 +1,33 @@
+//
+//  WoopEventTests.swift
+//  WoopEventTests
+//
+//  Created by Marcus Vinicius Grussner on 04/10/21.
+//
+
+import XCTest
+@testable import WoopEvent
+
+class WoopEventTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
